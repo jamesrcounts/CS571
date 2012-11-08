7222ebe76afa4e54af9c0cdd2ea2a2f0972add1b
James Counts <fritzjob18@nethere.com>
2012-11-08 13:07:00 -0800

    Replaced with 'Emergency' version of hw5.pl

--- HW5/Prolog/hw5.pl
+++ HW5/Prolog/hw5.pl
@@ -1,11 +1,8 @@
-% cs571 Yoshii - HW5 program
-
 eagle(bill).
-fly(mary).
 airplane(dc10).
-
 find(Who) :- wings(Who), write(Who).
 wings(X) :- fly(X).
+fly(mary).
 fly(Y) :- bird(Y).
-bird(Z) :- eagle(Z).
 fly(X) :- airplane(X).
+bird(Z) :- eagle(Z).
