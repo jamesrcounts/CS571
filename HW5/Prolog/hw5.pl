0d7df7b72d680e03dfe18b2f498e9891980c520b
James Counts <fritzjob18@nethere.com>
2012-11-08 13:04:41 -0800

    Added Facts and Rules to hw5.pl

--- HW5/Prolog/hw5.pl
+++ HW5/Prolog/hw5.pl
@@ -1,8 +1,11 @@
 % cs571 Yoshii - HW5 program
 
 eagle(bill).
+fly(mary).
+airplane(dc10).
 
+find(Who) :- wings(Who), write(Who).
 wings(X) :- fly(X).
 fly(Y) :- bird(Y).
 bird(Z) :- eagle(Z).
-
+fly(X) :- airplane(X).
